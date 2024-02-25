8dedd876148cc3a6a59ecf0fa429ca2cf90d9149
Victoria Edwards <142756923+VictoriaEdwards2@users.noreply.github.com>
2024-02-25 20:15:59 +0000

    Sun, Feb 25, 2024, 12:15 PM -08:00

---
+++ js/loading.js
@@ -0,0 +1,23 @@
+
+loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
+
+load = 0
+
+int = setInterval(blurring, 30)
+
+
+function blurring()
+{
+
+    load = load + 1
+    if(load > 99)
+    {
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
+
+
