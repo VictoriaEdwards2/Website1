97cd2deefd617c5b022965751f41cca8e8273df8
Victoria Edwards <142756923+VictoriaEdwards2@users.noreply.github.com>
2024-02-25 20:23:18 +0000

    Sun, Feb 25, 2024, 12:23 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -16,10 +16,8 @@ function blurring()
         clearInterval(int)
     }
     loadText.innerText = `${load}%`
-
-    
-    loadText.style.opacity = 1
-    bg.style.filter = "blur(10px)"
+    loadText.style.opacity = 1 - load/100
+    bg.style.filter = `blur(${30 - (load/100)*30}px)`
 }
 
 
